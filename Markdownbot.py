afca113a2f3084f4fa257f875ea50a416bbbe43b
Jerry Bai <xinyoubai@hotmail.com>
2025-11-12 11:04:28 -0800

    randomstuff

---
+++ Markdownbot.py
@@ -0,0 +1,17 @@
+print("Hi, I'm ChatBot!")
+
+name = input("What's your name? ")
+
+print(f"Nice to meet you, {name}!")
+
+color = input("What's your favorite color? ")
+hobby = input("What do you like to do for fun? ")
+food = input("What's your favorite food? ")
+
+# Respond to their answers
+print(f"Oh, {color} is such a nice color!")
+print(f"{hobby.capitalize()} sounds really fun!")
+print(f"I love {food} too! Yummy!")
+
+# Say goodbye
+print(f"It was great talking with you, {name}! Goodbye!")
